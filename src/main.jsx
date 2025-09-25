01bf8cc3c6bf2d4d4081cf2ce161ad434ccf6079
mustafijul <mustafijulislam21@gmail.com>
2025-09-25 14:25:38 +0600

    platform section done

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Navbar from './Shared/Navbar.jsx'
 import Home from './Home/Home.jsx';
 import Footer from './Shared/Footer.jsx';
 import Stats from './Stats/Stats.jsx';
+import Platform from './Platform/Platform.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')).render(
       <Navbar></Navbar>
       <Home></Home>
       <Stats></Stats>
+      <Platform></Platform>
       <Footer></Footer>
     </div>
   
